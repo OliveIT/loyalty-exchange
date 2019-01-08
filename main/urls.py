ab6b7c34c2a029e925d2ae6f8ef76aba50222310
OliveIT <olivedev92@gmail.com>
2019-01-08 16:27:59 +1000

    remove migration history, rename api urls

--- main/urls.py
+++ main/urls.py
@@ -15,14 +15,14 @@ urlpatterns = [
     url(r'^', include('services.urls')),
     url(r'^admin/', admin.site.urls),
 
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^rest-urls/', include('rest_framework.urls', namespace='rest_framework')),
     # url(r'^schema/$', schema_view),
     # url(r'^docs/', include_docs_urls(title=API_TITLE, description=API_DESCRIPTION))
     url(r'^swagger/$', get_swagger_view(title='API Docs'), name='api_docs'),
 
 # auth
-    url(r'^rest-auth/', include('rest_auth.urls')),
-    url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
+    url(r'^auth/', include('rest_auth.urls')),
+    url(r'^auth/registration/', include('rest_auth.registration.urls')),
     url(r'^account/', include('allauth.urls')),
     url(r'^accounts/profile/$', RedirectView.as_view(url='/', permanent=True), name='profile-redirect'),
 ]
