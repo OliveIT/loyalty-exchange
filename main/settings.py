f176177aeba47bb617aa9cf6e913da796891395c
OliveIT <olivedev92@gmail.com>
2019-02-01 01:18:26 +1000

    change console to smtp

--- main/settings.py
+++ main/settings.py
@@ -185,7 +185,7 @@ REST_SESSION_LOGIN = True
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_EMAIL_REQUIRED = True
